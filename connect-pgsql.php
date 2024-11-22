d7f5dd351174169fd4cd4d6582a505833741115c
ROSTAND LELE <rostoinfocus@gmail.com>
2024-11-22 12:56:11 +0100

    dese
    desr

---
+++ connect-pgsql.php
@@ -0,0 +1,13 @@
+<?php
+$dsn = 'pgsql:host=127.0.0.1;dbname=connect-pgsql';
+$username = 'postgres'; // Nom d'utilisateur modifié
+$password = '3231'; // Mot de passe
+$options = [];
+
+try {
+  $bdd = new PDO($dsn, $username, $password, $options);
+  echo "Connexion reussie a la base de donneesPostgres ";
+
+} catch(PDOException $e) {
+die("erreur: " . $e->getMessage());
+}
